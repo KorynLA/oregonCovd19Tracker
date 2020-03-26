612a34119a2e76b8032e225bbb4fa16b7adc956f
KorynLA <leslieak@oregonstate.edu>
2020-03-25 22:08:52 -0700

    Added cors to backend & query to endpoint

--- oregonCovd19_backend/app.js
+++ oregonCovd19_backend/app.js
@@ -2,31 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-
-//database connection
-var pg = require('pg');
-var conString = "postgres://:@localhost:5432/oregonCovd19";
-
-var client = new pg.Client(conString);
-client.connect();
-client.query('SELECT countyname, population FROM counties', (err, res) => {
-	if(err) {
-		console.log(err.stack);
-	}
-	else {
-		console.log(res.rows);
-	}
- })
+var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
