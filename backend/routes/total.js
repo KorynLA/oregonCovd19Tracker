a7b06bbfc55b636f6df091c4d0d93d81677c7302
KorynLA <leslieak@oregonstate.edu>
2020-05-25 08:53:21 -0700

    Adding test to route

--- backend/routes/total.js
+++ backend/routes/total.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 var client = require('../dbhandler');
 
-client.connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log("Test");
+	client.connect();
+	console.log("connecting");
 	// promise
+	console.log("getting");
 	client.query("SELECT distinct date_of_cases, positive_cases, deaths FROM Total ORDER BY date_of_cases DESC")
   	.then(response => {
       res.send(JSON.stringify(response.rows));
